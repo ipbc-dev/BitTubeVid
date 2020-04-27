a079bf4274b4b0b7bdf269870e53a97743d7822c
ICabaleiro <i.cabaleiro@devcups.com>
2020-04-27 13:13:24 +0100

    Add metadata

--- server/helpers/ffmpeg-utils.ts
+++ server/helpers/ffmpeg-utils.ts
@@ -497,7 +497,7 @@ async function presetH264 (command: ffmpeg.FfmpegCommand, input: string, resolut
     //.outputOption('-b_strategy 1') // NOTE: b-strategy 1 - heuristic algorithm, 16 is optimal B-frames for it
     //.outputOption('-bf 16') // NOTE: Why 16: https://github.com/Chocobozzz/PeerTube/pull/774. b-strategy 2 -> B-frames<16
     //.outputOption('-pix_fmt yuv420p') // allows import of source material with incompatible pixel formats (e.g. MJPEG video)
-    //.outputOption('-map_metadata -1') // strip all metadata
+    .outputOption('-map_metadata -1') // strip all metadata
     //.outputOption('-movflags faststart')
 
   const parsedAudio = await audio.get(input)
