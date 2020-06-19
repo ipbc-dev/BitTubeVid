bf8baa6d2439e7f60c880dc49b59839325be93cd
ICabaleiro <i.cabaleiro@devcups.com>
2020-06-19 17:50:52 +0100

    Fixing variable bug

--- server/helpers/webtorrent.ts
+++ server/helpers/webtorrent.ts
@@ -78,7 +78,7 @@ async function downloadWebTorrentVideo (target: { magnetUri: string, torrentName
   })
 }
 
-async function createTorrentAndSetInfoHash (videoOrPlaylist: MVideo | MStreamingPlaylistVideo, videoFile: MVideoFile, videoCounter = 0, useTemporalFile = true) {
+async function createTorrentAndSetInfoHash (videoOrPlaylist: MVideo | MStreamingPlaylistVideo, videoFile: MVideoFile, videoCounter = 0, useTemporalFile = false) {
   let auxTime = Date.now()
   const video = extractVideo(videoOrPlaylist)
   logger.info(`ICEICE ${videoCounter} after extractVideo ${(Date.now() - auxTime) / 1000} sec`)
