5ea324b06a3f9fcf5fc45fe8cd6539cc6f0cddeb
ICabaleiro <i.cabaleiro@devcups.com>
2020-07-02 08:26:34 +0100

    Adding component my account storage billing

---
+++ client/src/app/+my-account/my-account-storage-billing/my-account-storage-billing.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core'
+import { Notifier } from '@app/core'
+import { I18n } from '@ngx-translate/i18n-polyfill'
+import { RestPagination, RestTable } from '@app/shared'
+import { SortMeta } from 'primeng/api'
+import { AccountBlock, BlocklistService } from '@app/shared/blocklist'
+
+@Component({
+  selector: 'my-account-storage-billing',
+  styleUrls: [ './my-account-storage-billing.component.scss' ],
+  templateUrl: './my-account-storage-billing.component.html'
+})
+export class MyAccountStorageBillingComponent extends RestTable implements OnInit {
+  blockedAccounts: AccountBlock[] = []
+  totalRecords = 0
+  sort: SortMeta = { field: 'createdAt', order: -1 }
+  pagination: RestPagination = { count: this.rowsPerPage, start: 0 }
+
+  constructor (
+    private notifier: Notifier,
+    private blocklistService: BlocklistService,
+    private i18n: I18n
+  ) {
+    super()
+  }
+
+  ngOnInit () {
+    this.initialize()
+  }
+
+  getIdentifier () {
+    return 'MyAccountStorageBillingComponent'
+  }
+
+  unblockAccount (accountBlock: AccountBlock) {
+    const blockedAccount = accountBlock.blockedAccount
+
+    this.blocklistService.unblockAccountByUser(blockedAccount)
+        .subscribe(
+          () => {
+            this.notifier.success(this.i18n('Account {{nameWithHost}} unmuted.', { nameWithHost: blockedAccount.nameWithHost }))
+
+            this.loadData()
+          }
+        )
+  }
+
+  protected loadData () {
+    return this.blocklistService.getUserAccountBlocklist(this.pagination, this.sort)
+      .subscribe(
+        resultList => {
+          this.blockedAccounts = resultList.data
+          this.totalRecords = resultList.total
+        },
+
+        err => this.notifier.error(err.message)
+      )
+  }
+}
